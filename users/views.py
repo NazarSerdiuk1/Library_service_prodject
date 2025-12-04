31ffdf8b35f4937f66787090c6e0d6022aa3325e
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2025-12-04 15:03:51 +0200

    Create tests for the project

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
